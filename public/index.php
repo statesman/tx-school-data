a7b55d82a57e3d893a7b159331eadce45d396487
Dan Hill <danhillreports@gmail.com>
2016-08-15 15:41:45 -0500

    Update not rated variable

--- public/index.php
+++ public/index.php
@@ -153,7 +153,7 @@
               </div>
             <% } else if (ratingYear.rating === "X" || ratingYear.rating === "Z") { %>
               <div class="muted-label">
-                  <p class="hed"><%= year %>&ensp;<i class="fa fa-minus-circle"></i></p>
+                  <p class="hed"><%= ratingYear.year %>&ensp;<i class="fa fa-minus-circle"></i></p>
                   <p class="txt">Not rated</p>
               </div>
             <% }
