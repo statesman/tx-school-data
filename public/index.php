eb7dc9c852ab08e4d97ec93bef2b5a4974fc43d9
Cody Winchester <cjwinchester@gmail.com>
2016-08-16 09:37:48 -0500

    no bar charts, just labels

--- public/index.php
+++ public/index.php
@@ -141,6 +141,8 @@
           <h2><%= school.name  %></h2>
           <h3 class="subhed"><small><%= school.dist_name %></small></h3>
 
+          <div class="section topline">
+          <h3>Overall ratings</h3>
           <div class="year-labels">
           <% _.each(school.ratings, function(ratingYear) {
             if (ratingYear.rating === "M") { %>
@@ -167,17 +169,8 @@
             });
           %>
           </div>
+      </div>
           <div class="section categories">
-              <div class="legend-row row">
-                <div class="col-sm-6">
-                  <div class="legend-text col-xs-12"><p><strong>Legend:</strong></p></div>
-                  <div class="col-xs-12 bar-goal-legend">&emsp;TEA target score</div>
-                  <div class="col-xs-4 bar-value-legend"></div><div class="col-xs-8">Score given to school</div>
-                </div>
-                <div class="col-sm-6">
-                  <div class="col-xs-12"></div>
-                </div>
-              </div>
 
               <% _.each(["Student Achievement (Index 1)","Student Progress (Index 2)","Closing Performance Gaps (Index 3)","Postsecondary Readiness (Index 4)"],
                       function(title, i) { %>
@@ -187,41 +180,30 @@
                 "Student performance improvement.",
                 "Academic achievement of economically disadvantaged students and the two lowest-performing racial student groups.",
                 "Measure of a school’s ability to prepare its students for the next level of education, or the workforce."][i] %></p>
-              <div class="index-chart">
-
-                  <div class="row">
-                      <div class="col-xs-10 col-xs-offset-2">
-                          <div class="bar-scale-wrapper">
-                              <span class="small muted pull-left">0</span>
-                              <span class="small muted pull-right">100</span>
-                              <div class="clearfix"></div>
-                              <div class="bar-scale"></div>
-                          </div>
+                  <% _.each(school[i + 1].scores, function(scoreYear) {
+                    if (scoreYear.score === null) { %>
+                      <div class="muted-label">
+                          <p class="hed"><%= scoreYear.year %>&ensp;<i class="fa fa-minus-circle"></i></p>
+                          <p class="txt">Not rated</p>
                       </div>
-                  </div>
-
-                  <div class="clearfix"></div>
-
-                  <% _.each(school[i + 1].scores, function(scoreYear) { %>
-                  <div class="row bar-group">
-                      <div class="col-xs-2 bar-year-label">
-                          <%= scoreYear.year %>
+                    <% } else if (scoreYear.score < scoreYear.target) { %>
+                      <div class="muted-label text-danger">
+                          <p class="hed"><%= scoreYear.year %>&ensp;<i class="fa fa-times-circle"></i></p>
+                          <p class="txt">Did not meet standard</p>
+                          <p class="txt bold">Score: <%= scoreYear.score %></p>
+                          <p class="txt">Target: <%= scoreYear.target %></p>
                       </div>
-                      <div class="col-xs-10">
-                          <% if (scoreYear.score === null) { %>
-                          <div class="bar italic" style="margin-top:2px;">No data this year</div>
-                          <% } else { %>
-                          <div class="bar">
-                              <div class="bar-value" style="width: <%= scoreYear.score %>%;"></div>
-                              <div class="bar-goal" style="width: <%= scoreYear.target %>%;"></div>
-                          </div>
-                          <% } %>
+                    <% } else if (scoreYear.score >= scoreYear.target) { %>
+                      <div class="muted-label text-success">
+                          <p class="hed"><%= scoreYear.year %>&ensp;<i class="fa fa-check-circle"></i></p>
+                          <p class="txt">Met standard</p>
+                          <p class="txt bold">Score: <%= scoreYear.score %></p>
+                          <p class="txt">Target: <%= scoreYear.target %></p>
                       </div>
-                  </div>
-                  <% }); %>
-              </div>
+                  <% }; %>
               <% }); %>
           </div>
+<% }); %>
           </div>
       </div>
   </script>
