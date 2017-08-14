865922c34ca731f8aec26ea2eb15d345c561b41b
Dan Keemahill <danhillreports@gmail.com>
2017-08-14 16:25:33 -0500

    Exclude public/assets/data from main publish tasks
    Moves directory during upload and moves back. Resolves #5

--- tasks/deployS3.js
+++ tasks/deployS3.js
@@ -4,6 +4,8 @@ module.exports = (grunt) => {
   grunt.registerTask('deployS3', 'Deploy to s3.', function(destination) {
     var done = this.async();
 
+    const fs = require('fs');
+
     const s3 = require('s3');
     const awsAccess = process.env.AWS_ACCESS_KEY_ID;
     const awsSecret = process.env.AWS_SECRET_ACCESS_KEY;
@@ -11,6 +13,13 @@ module.exports = (grunt) => {
     const config = require("../project.config");
 
     const s3_bucket = destination === "prod" ? "apps.statesman.com" : "dev.apps.statesman.com";
+
+    // Move data directory before upload
+    fs.rename(
+      'public/assets/data/',
+      'tmp_data_dir',
+      () => {return;}
+    );
 
     // check for AWS environmental variables
     if (awsAccess && awsSecret) {
@@ -52,9 +61,21 @@ module.exports = (grunt) => {
       });
       uploader.on('end', function() {
         grunt.log.ok("🐿  🐿  🐿  you deployed to " + s3_bucket);
+        // Replace data directory after upload
+        fs.rename(
+          'tmp_data_dir',
+          'public/assets/data/',
+          () => {return;}
+        );
         done();
       });
     } else {
+        // Replace data directory after upload
+        fs.rename(
+          'tmp_data_dir',
+          'public/assets/data/',
+          () => {return;}
+        );
       grunt.fatal('You\'re missing one or both AWS credential environmental variables.');
       done(Error);
     }
